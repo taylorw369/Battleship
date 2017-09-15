e57e4cfde3b81a03ca4273839a170e7f74954e08
sdilts <stuart.dilts@gmail.com>
2017-09-15 09:44:23 -0600

    Fixed various errors

--- curlCommand.sh
+++ curlCommand.sh
@@ -1,1 +1,3 @@
-curl -X POST "http://0.0.0.0:5000/fire?x=0&y=1"
+#!/bin/bash
+
+curl -i -X POST "http://0.0.0.0:5000/fire?x=0&y=1"
--- server.py
+++ server.py
@@ -1,48 +1,57 @@
 import random
-from flask import Flask, render_template, Response, request
+import sys
+from flask import Flask, render_template, Response, request, make_response, send_from_directory
 
 app = Flask(__name__)
+
+port = sys.argv[1]
+own_board = sys.argv[2]
 
 def save_board(board, file_name):
     with open(file_name, "w") as f:
         for line in board:
-            f.write(line)
+            f.write("".join(line) + "\n")
 
 def read_board(file_name):
-    F = open(file_name, "r")
     board = []
-    for line in F:
-        board.append(line)
-    F.close()
+    with open(file_name, "r") as f:
+        print(f)
+        for line in f:
+            board.append(list(line.rstrip(' \n')))
     return board
 
-def showHTML():
-    doStuff()
+# # Hacky hacky hacky!
+@app.route('/<path:path>')
+def showHTML(path):
+     return render_template(path, board=read_board()
+
 
 #fired upon
 #/fire?x=5&y=4
-@app.route('/fire', methods = ['GET','POST'])
+@app.route('/fire', methods = ['POST'])
 def update_own_board():
-    x = request.args.get('x')
-    y = request.args.get('y')
+    x = int(request.args.get('x'))
+    y = int(request.args.get('y'))
 
-    print("x = " + x + ", y = " + y)
+    board = read_board(own_board)
+
+    print("x = %d, y = %d" % (x, y))
     # if out of bounds?
     if x >= 10 or y >= 10 or x < 0 or y < 0:
-        return make_response("Out of bounds", 404)
+        return make_response("Out of bounds\n", 404)
     # spot has already been fired on
-    elif board[x][y] == 'H' or board[x][y] == 'M':
-        return make_response("Already fired on this spot", 410)
+    elif board[y][x] == 'H' or board[y][x] == 'M':
+        return make_response("Already fired on this spot\n", 410)
     # they missed the dang thing:
-    elif board[x][y] == '_':
-        board[x][y] = 'M'
-        save_board(board, "board.txt")
+    elif board[y][x] == '_':
+        board[y][x] = 'M'
+        save_board(board, own_board)
         return make_response("hit=0", 200)
     # they hit:
     else:
-        old = board[x][y]
-        board[x][y] = 'H'
-        save_board(board, "board.txt")
+        old = board[y][x]
+        board[y][x] = 'H'
+        save_board(board, own_board)
         numb = count_occurences(old, board)
         if numb == 0:
             # we sunk
@@ -72,35 +81,12 @@ def update_opponent_board():
 
     # no error checking!
     if sink != 0:
-        board[x][y] = sink
+        board[y][x] = sink
     else:
-        board[x][y] = info
+        board[y][x] = info
 
     save_board(board, "opponent_board.txt")
     return make_response("Board updated", 200)
-
-
-def save_board(board, file_name):
-    with open(file_name, "w") as f:
-        for line in board:
-            f.write(line)
-
-def read_board(file_name):
-    F = open(file_name, "r")
-    board = []
-    for line in F:
-        board.append(line)
-    F.close()
-    return board
-
-
-def doStuff():
-    print("Not implemented yet")
-
-doStuff()
-
-
-
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0')
